6f7ee339540a60168cc15524f0e0a51a6fdefd41
danielorlowski00 <danielorlowski00@gmail.com>
2023-07-03 21:17:36 +0200

    Upload dockerfiles for deployment

--- Zadanie 5/backend/src/main/kotlin/com/example/Application.kt
+++ Zadanie 5/backend/src/main/kotlin/com/example/Application.kt
@@ -9,7 +9,7 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, System.getenv("PORT").toInt(), host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
